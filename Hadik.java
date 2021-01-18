70fdb23deccf0e06aad82c706515aa96dc3997a6
DanielKlimek <73240376+DanielKlimek@users.noreply.github.com>
2021-01-18 21:49:07 +0100

    Add files via upload

---
+++ Hadik.java
@@ -0,0 +1,19 @@
+
+package hadik;
+
+
+public class Hadik {
+
+public bod hlava;
+
+public char smer;
+
+public void init(int x, int y){
+    this.hlava      = new bod();
+    this.hlava.x    = x;
+    this.hlava.y    = y;
+    this.smer = 'w';
+}
+}
+    
+
